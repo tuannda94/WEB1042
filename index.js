729045f619e74d154524cd5acbd01a9c1e56a332
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-06 17:58:20 +0700

    Buoi 1

---
+++ index.js
@@ -0,0 +1,18 @@
+// Khi viết sang file .js thì chỉ có cú pháp js
+// alert('Hello index.js');
+
+// 1. Khai báo biến
+var ten_bien; // KDL undefined
+var ten_bien_2 = 123456; // KDL số
+ten_bien_2 = 'abcdef'; // KDL chữ
+console.log(ten_bien, ten_bien_2);
+// type of
+console.log(typeof ten_bien, typeof ten_bien_2);
+
+var bien_boolean = true; // true/false KDL boolean
+var bien_null = null; // KDL null
+var bien_array = [1,2,3]; // KDL mảng
+var bien_object = {}; // KDL đối tượng
+
+console.log(typeof bien_boolean == 'boolean');
+
