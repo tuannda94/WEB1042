33ccf0d99d4c1213e8dd9f20f2af32d5aade4604
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-07 13:00:39 +0700

    Buoi 1

---
+++ index.js
@@ -0,0 +1,25 @@
+// Nơi code js và đính vào thẻ <script src
+// alert('Thong bao o index.js');
+
+// Khai báo
+var ten_bien; // undefined
+var ten_bien_2 = 123; // 123
+// Để nhìn lấy console log thì mở Devtool ở trình duyệt
+// trong trang html chứa phần js này -> tab Console
+console.log(ten_bien, ten_bien_2);
+
+// Kiểu dữ liệu
+var bien_undefined; // không gán gtri là undefined
+var bien_so = 123; // số
+var bien_chuoi = 'abc'; // chuỗi
+var bien_null = null;
+var bien_boolean = true; // true/false
+var bien_mang = [1, 2, 3]; // mảng
+var bien_doi_tuong = {}; // đối tượng
+
+// typeof
+console.log('bien_undefined: ', typeof bien_undefined);
+console.log('null: ', typeof bien_null); // object
+console.log('boolean: ', typeof bien_boolean); // boolean
+console.log('mang: ', typeof bien_mang); // object
+console.log('doi tuong: ', typeof bien_doi_tuong); // object
