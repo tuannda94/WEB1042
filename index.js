a1ee9b824913c4707e4a905ddcb8ecce2c9533f3
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-07 09:10:47 +0700

    Buoi 1

---
+++ index.js
@@ -0,0 +1,19 @@
+// Nơi viết cú pháp javascript mà k cần dùng thẻ html là <script>
+// alert('Hello index.js');
+
+// alert(ten_bien_2);
+
+var bien_1 = 123;
+bien_1 = 'abc';
+
+// log giá trị ra kiểm tra
+// Bấm F12 ở trình duyệt khi đang mở file index.html -> Console
+console.log('Gia tri cua bien 1: ', bien_1);
+
+// typeof
+console.log('Kieu du lieu cua bien 1: ', typeof bien_1);
+console.log('Kieu du lieu cua undefined: ', typeof undefined);
+console.log('Kieu du lieu cua null: ', typeof null);
+console.log('Kieu du lieu cua true: ', typeof true);
+console.log('Kieu du lieu cua array: ', typeof [1,2,3]);
+console.log('Kieu du lieu cua object: ', typeof {});
