fbd3ab39618b1bcaed6aeaa579fc4d3f502a1e19
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2023-03-07 17:15:58 +0700

    Buoi 1

---
+++ index.js
@@ -0,0 +1,22 @@
+// alert('File index.js');
+
+// Khai báo biến
+var ten_bien; // 0, null,
+var ten_bien_2 = 123;
+// Mở trình duyệt -> Bấm F12 ở trang html này
+// Chuyển sang tab Console
+console.log(ten_bien, ten_bien_2);
+
+var bien_so = 123456;
+var bien_chuoi = 'abc';
+var bien_boolean = true; // true/false
+var bien_mang = [1,2,3];
+var bien_doi_tuong = {};
+var bien_null = null;
+
+console.log('Kieu DL so: ', typeof bien_so);
+console.log('Kieu DL chuoi: ', typeof bien_chuoi);
+console.log('Kieu DL boolean: ', typeof bien_boolean);
+console.log('Kieu DL mang: ', typeof bien_mang);
+console.log('Kieu DL dt: ', typeof bien_doi_tuong);
+console.log('Kieu DL null: ', typeof bien_null);
